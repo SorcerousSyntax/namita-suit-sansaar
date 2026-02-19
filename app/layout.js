953dc40befc4ed5fbe91f6f364709e502a0a77e0
SorcerousSyntax <cgharsh6750@gmail.com>
2026-02-19 21:29:59 +0530

    Debug: Comment out layout components to isolate build error

--- app/layout.js
+++ app/layout.js
@@ -21,14 +21,14 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body>
-                <AuthProvider>
-                    <CartProvider>
-                        <ToastProvider>
-                            <Navbar />
-                            <main>{children}</main>
-                        </ToastProvider>
-                    </CartProvider>
-                </AuthProvider>
+                {/* <AuthProvider> */}
+                {/* <CartProvider> */}
+                {/* <ToastProvider> */}
+                {/* <Navbar /> */}
+                <main>{children}</main>
+                {/* </ToastProvider> */}
+                {/* </CartProvider> */}
+                {/* </AuthProvider> */}
             </body>
         </html>
     );
